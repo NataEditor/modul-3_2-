75e9bbf7454bd49e77195cdd3ae4ae4e6d8bcb7b
NataEditor <nako_al@mail.ru>
2024-08-19 16:12:46 +0300

    Update module_3_2.py
    был исправлен

--- module_3_2.py
+++ module_3_2.py
@@ -1,19 +1,31 @@
 
 
 
-def send_email(message, recipient,sender = "university.help@gmail.com"):
-    if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
+
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    if "@" not in (recipient or sender):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    elif recipient == sender:
-        print("Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    elif sender != "university.help@gmail.com":
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+        exit()
+    if recipient.endswith('.ru') + recipient.endswith('.com') + recipient.endswith('.net') != True:
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        exit()
+    if sender.endswith('.ru') + sender.endswith('.com') + sender.endswith('.net') != True:
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        exit()
+    else:
+        if recipient == sender:
+                print("Нельзя отправить письмо самому себе!")
+                exit()
+        if sender == "university.help@gmail.com":
+                print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+                exit()
+        if sender != "university.help@gmail.com":
+            print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+            exit()
 
 
 
-
-send_email('hi', 'help@h.com')
 send_email('hi', 'university.help@gmail.com')
-send_email('hi', 'university.help@gmail.com', 'help@h.com')
+
+
+
